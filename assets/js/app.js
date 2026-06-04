04ab53b38816041d7df287176226baecb3580477
howkinss@gmail.com <howkinss@gmail.com>
2026-06-04 10:02:12 +0300

    cookie zenodo popup

--- assets/js/app.js
+++ assets/js/app.js
@@ -435,10 +435,41 @@ $(document).ready(function() {
 
 });
 
+function setZenodoCookie(name, value, days){
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getZenodoCookie(name){
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 function openZenodoModalBtn(){
+    // Don't display the popup again once it has been closed
+    if (getZenodoCookie("zenodoPopupClosed")) {
+        return;
+    }
+
     setTimeout(function() {
         $(".openZenodoModalBtn").trigger("click");
     },10);
+
+    // When the popup is closed, set a cookie so it won't be shown anymore
+    $('#content-confirmation').on('hidden.bs.modal', function() {
+        setZenodoCookie("zenodoPopupClosed", "1", 365);
+    });
 }
 function onHashChange(){
 	$("path").removeClass('active_path');
